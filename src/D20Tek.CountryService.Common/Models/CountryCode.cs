98132470a25836ea58076d9257f318ad31e53598
Pedro Silva <pedrosi@microsoft.com>
2022-05-12 21:40:45 -0700

    Added shared project for common model classes and shared implementations.

---
+++ src/D20Tek.CountryService.Common/Models/CountryCode.cs
@@ -0,0 +1,23 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+
+namespace D20Tek.CountryService.Models
+{
+    public class CountryCode
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public int Sovereignty { get; set; }
+
+        public string Alpha2Code { get; set; } = string.Empty;
+
+        public string Alpha3Code { get; set; } = string.Empty;
+
+        public int CountryId { get; set; }
+
+        public string TopLevelDomain { get; set; } = string.Empty;
+    }
+}
---
+++ tests/D20Tek.CountryService.UnitTests/Models/CountryCodeTests.cs
@@ -0,0 +1,37 @@
+﻿using D20Tek.CountryService.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace D20Tek.CountryService.UnitTests.Models
+{
+    [TestClass]
+    public class CountryCodeTests
+    {
+        [TestMethod]
+        public void Create()
+        {
+            // arrange
+
+            // act
+            var code = new CountryCode
+            {
+                CountryId = 101,
+                Name = "Test",
+                FullName = "Testlandia",
+                Alpha2Code = "TS",
+                Alpha3Code = "TST",
+                Sovereignty = 0,
+                TopLevelDomain = ".ts",
+            };
+
+            // assert
+            Assert.IsNotNull(code);
+            Assert.AreEqual(101, code.CountryId);
+            Assert.AreEqual("Test", code.Name);  
+            Assert.AreEqual("Testlandia", code.FullName);  
+            Assert.AreEqual("TS", code.Alpha2Code);
+            Assert.AreEqual("TST", code.Alpha3Code);
+            Assert.AreEqual(0, code.Sovereignty);
+            Assert.AreEqual(".ts", code.TopLevelDomain);
+        }
+    }
+}
